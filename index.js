9cb08dfe281830017d039a6252deedba8b5085ac
Subodh <subodhlst@gmail.com>
2025-08-10 03:53:16 +0530

    fill the template

--- index.js
+++ index.js
@@ -32,21 +32,68 @@ const url = require("url");
 ///////////////////////////////////////////////////////////////
 //SERVER
 
+const replaceTemplate = function (temp, product) {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%FROM%}/g, product.from);
+
+  if (!output.organic) output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
+
+  //Overview
   if (pathName === "/" || pathName === "/overview") {
-    res.end("this is the Overview");
-  } else if (pathName === "/product") {
+    res.writeHead(200, { "content-type": "text/html" });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    console.log(cardsHtml);
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+    res.end(output);
+  }
+
+  //Product
+  else if (pathName === "/product") {
     res.end("This is the Product");
-  } else if (pathName === "/api") {
+  }
+
+  //Api
+  else if (pathName === "/api") {
     res.writeHead(200, {
       "content-type": "application/json",
     });
     res.end(data);
-  } else {
+  }
+
+  //Page not found
+  else {
     res.writeHead(404, {
       "content-type": "text/html",
       "my-own-header": "hello world",
