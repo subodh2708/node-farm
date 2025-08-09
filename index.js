57f53d50e8cd2ab1a14187933cc20b0ae2bf1e7b
Subodh <subodhlst@gmail.com>
2025-08-10 01:42:35 +0530

    build simple api

--- index.js
+++ index.js
@@ -32,18 +32,26 @@ const url = require("url");
 ///////////////////////////////////////////////////////////////
 //SERVER
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   if (pathName === "/" || pathName === "/overview") {
     res.end("this is the Overview");
   } else if (pathName === "/product") {
     res.end("This is the Product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "content-type": "text/html",
       "my-own-header": "hello world",
     });
-    res.end("<h1>Page not found!</h1>");
+    res.end("<h1>Page not found!</h1> ");
   }
 });
 
