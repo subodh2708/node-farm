0584979870a9a5c0d6654527479f882e6bd9beb7
Subodh <subodhlst@gmail.com>
2025-08-10 05:48:14 +0530

    add own  module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const http = require("http");
 const path = require("path");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 ///////////////////////////////////////////////////
 //FILE
@@ -32,23 +33,6 @@ const url = require("url");
 ///////////////////////////////////////////////////////////////
 //SERVER
 
-const replaceTemplate = function (temp, product) {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-};
-
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
@@ -67,7 +51,6 @@ const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
-  console.log(query);
 
   //Overview
   if (pathname === "/" || pathname === "/overview") {
